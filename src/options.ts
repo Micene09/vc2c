caf95892d038136f18c8102ddd92af0c0b7cc76a
micene09 <giovanni.contino09@gmail.com>
2022-07-15 20:54:05 +0200

    fixed quotes

--- src/options.ts
+++ src/options.ts
@@ -19,7 +19,7 @@ export type InputVc2cOptions = Partial<Vc2cOptions>
 
 export function getDefaultVc2cOptions (tsModule: typeof ts = ts): Vc2cOptions {
   return {
-    root: "process" in globalThis ? process.cwd() : "",
+    root: 'process' in globalThis ? process.cwd() : '',
     debug: false,
     compatible: false,
     setupPropsKey: 'props',
