ab7e3decb64101bef0fe8a3cab185faa2b702110
micene09 <giovanni.contino09@gmail.com>
2022-07-15 21:22:28 +0200

    update debug mode

--- demo/index.ts
+++ demo/index.ts
@@ -1,35 +1,35 @@
-import * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor'
 import { ScriptTarget } from 'typescript'
 import { convert } from '../src/index'
 
 (self as any).MonacoEnvironment = {
-	getWorker: function (workerId: string, label: string) {
-		const getWorkerModule = (moduleUrl: string, label: string) => {
-			return new Worker((self as any).MonacoEnvironment.getWorkerUrl(moduleUrl), {
-				name: label,
-				type: 'module'
-			});
-		};
+  getWorker: function (workerId: string, label: string) {
+    const getWorkerModule = (moduleUrl: string, label: string) => {
+      return new Worker((self as any).MonacoEnvironment.getWorkerUrl(moduleUrl), {
+        name: label,
+        type: 'module'
+      })
+    }
 
-		switch (label) {
-			case 'json':
-				return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
-			case 'css':
-			case 'scss':
-			case 'less':
-				return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
-			case 'html':
-			case 'handlebars':
-			case 'razor':
-				return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
-			case 'typescript':
-			case 'javascript':
-				return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
-			default:
-				return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
-		}
-	}
-};
+    switch (label) {
+      case 'json':
+        return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label)
+      case 'css':
+      case 'scss':
+      case 'less':
+        return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label)
+      case 'html':
+      case 'handlebars':
+      case 'razor':
+        return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label)
+      case 'typescript':
+      case 'javascript':
+        return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label)
+      default:
+        return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label)
+    }
+  }
+}
 
 const defaultCode = `import Vue from 'vue'
 import { Prop, Component, Ref, Model, Provide, Inject } from 'vue-property-decorator'
@@ -85,58 +85,54 @@ export default class BasicPropertyClass extends Vue {
   }
 }`
 
-const vc2cConfig = {
-	compatible: false
-}
+const vc2cConfig = {}
 
 monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-	experimentalDecorators: true,
-	noResolve: true,
-	target: ScriptTarget.ESNext,
-	allowNonTsExtensions: true,
-	noEmit: true,
-	lib: [
-		'esnext',
-		'dom',
-		'dom.iterable',
-		'scripthost'
-	],
-	module: "esnext" as any,
-	strict: true,
-	esModuleInterop: true,
-	resolveJsonModule: true
+  experimentalDecorators: true,
+  noResolve: true,
+  target: ScriptTarget.ESNext,
+  allowNonTsExtensions: true,
+  noEmit: true,
+  lib: [
+    'esnext',
+    'dom',
+    'dom.iterable',
+    'scripthost'
+  ],
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  module: 'esnext' as any,
+  strict: true,
+  esModuleInterop: true,
+  resolveJsonModule: true
 })
 
 const editor = monaco.editor.create(document.getElementById('editor')!, {
-	value: defaultCode,
-	language: 'typescript',
-	theme: 'vs-dark',
-	minimap: {
-		enabled: false
-	}
+  value: defaultCode,
+  language: 'typescript',
+  theme: 'vs-dark',
+  minimap: {
+    enabled: false
+  }
 })
 
 const output = monaco.editor.create(document.getElementById('output')!, {
-	value: convert(defaultCode, vc2cConfig),
-	language: 'typescript',
-	theme: 'vs-dark'
+  value: convert(defaultCode, vc2cConfig),
+  language: 'typescript',
+  theme: 'vs-dark',
+  minimap: {
+    enabled: false
+  }
 })
 
 const setOutput = () => {
-	output.setValue(convert(editor.getValue(), vc2cConfig))
+  output.setValue(convert(editor.getValue(), vc2cConfig))
 }
 
 editor.onDidChangeModelContent(() => {
-	setOutput()
+  setOutput()
 })
 
 window.addEventListener('resize', () => {
-	editor.layout()
-	output.layout()
-})
-
-const compatibleCheckbox = document.getElementById('compatible') as HTMLInputElement
-compatibleCheckbox.addEventListener('change', () => {
-	vc2cConfig.compatible = compatibleCheckbox.checked
-	setOutput()
+  editor.layout()
+  output.layout()
 })
